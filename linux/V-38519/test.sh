917dea67f50c33a116dddd7470fae02b4d3b6ab7
Taylor C. Richberger <Taywee@gmx.com>
2015-08-07 16:30:54 -0600

    Fix grep filename printing

--- linux/V-38519/test.sh
+++ linux/V-38519/test.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 exitstatus=0
 
-for logfile in $(grep -roE '/[-\/a-zA-Z\._0-9]+($|[[:space:]])' /etc/rsyslog.*); do
+for logfile in $(grep -hroE '/[-\/a-zA-Z\._0-9]+($|[[:space:]])' /etc/rsyslog.*); do
     if ! test_group "$logfile" root; then
         exitstatus=1
     fi
