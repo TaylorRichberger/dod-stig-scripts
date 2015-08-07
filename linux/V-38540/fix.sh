c0194bc2d1c13eb34b5536450b70f4aad62fad42
Taylor C. Richberger <Taywee@gmx.com>
2015-08-07 17:00:50 -0600

    set arch

--- linux/V-38540/fix.sh
+++ linux/V-38540/fix.sh
@@ -2,7 +2,7 @@
 
 cat >> /etc/audit/audit.rules <<"HERE"
 # audit_network_modifications
--a always,exit -F arch=ARCH -S sethostname -S setdomainname -k audit_network_modifications
+-a always,exit -F arch=b64 -S sethostname -S setdomainname -k audit_network_modifications
 -w /etc/issue -p wa -k audit_network_modifications
 -w /etc/issue.net -p wa -k audit_network_modifications
 -w /etc/hosts -p wa -k audit_network_modifications
