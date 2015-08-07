1e26bf1387f936e6c7ffb9c90fee1a9a7a8cbb28
Taylor C. Richberger <Taywee@gmx.com>
2015-08-07 17:52:42 -0600

    typo

--- linux/V-38586/fix.sh
+++ linux/V-38586/fix.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if grep -qE '^SINGLE=' /etc/sysconfig/init; then
-    sed -i 's/^SINGLE=.*/SINGLE=\/sbin\/sulogon/' /etc/sysconfig/init
+    sed -i 's/^SINGLE=.*/SINGLE=\/sbin\/sulogin/' /etc/sysconfig/init
 else
-    echo 'SINGLE=/sbin/sulogon' >> /etc/sysconfig/init
+    echo 'SINGLE=/sbin/sulogin' >> /etc/sysconfig/init
 fi
