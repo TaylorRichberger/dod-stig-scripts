af180c2038edb85a990b07440c558b5b2da434d1
Taylor C. Richberger <Taywee@gmx.com>
2015-08-07 15:50:59 -0600

    filter out config files

--- linux/V-38447/test.sh
+++ linux/V-38447/test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ -n "$(rpm -Va | awk '$1 ~ /..5/ && $2 != "c"')" ]; then
+if [ -n "$(rpm -Va | awk '$1 ~ /..5/ && $2 != "c"' | grep -vE '.conf$')" ]; then
     echo 'There is a file with an unexpected hash:'
     rpm -Va | awk '$1 ~ /..5/ && $2 != "c"'
     exit 1
