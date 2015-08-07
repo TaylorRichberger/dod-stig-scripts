c344c3046b3fce506f2f652a4bc6135c54059373
Taylor C. Richberger <Taywee@gmx.com>
2015-08-07 15:49:35 -0600

    typo fixes

--- linux/V-38445/test.sh
+++ linux/V-38445/test.sh
@@ -3,7 +3,7 @@
 exitstatus=0
 
 for log in $(grep "^log_file" /etc/audit/auditd.conf | sed s/^[^\/]*//); do
-    if ! ./log/test_group "$log" root; then
+    if ! test_group "$log" root; then
         exitstatus=1
     fi
 done
