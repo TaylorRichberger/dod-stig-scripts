2056bde690aa33fac7f0ed59325317c6541bd265
Taylor C. Richberger <Taywee@gmx.com>
2015-08-07 17:43:08 -0600

    add new fix

--- linux/V-38575/fix.sh
+++ linux/V-38575/fix.sh
@@ -1,14 +1,6 @@
 #!/bin/sh
 
-bits=32
-
-if uname -p | grep -qF 64; then
-    bits=64
-fi
-
-cat >>/etc/audit.rules <<HERE
--a always,exit -F arch=b$bits -S unlink -S unlinkat -S rename -S renameat -F auid>=500 -F auid!=4294967295 -k delete
--a always,exit -F arch=b$bits -S unlink -S unlinkat -S rename -S renameat -F auid=0 -k delete
-HERE
-
-auditctl -R /etc/audit/audit.rules
+fix_audit_watch_syscall_uid unlink delete
+fix_audit_watch_syscall_uid unlinkat delete
+fix_audit_watch_syscall_uid rename delete
+fix_audit_watch_syscall_uid renameat delete
