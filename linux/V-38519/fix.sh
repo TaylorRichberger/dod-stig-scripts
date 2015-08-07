b8bd71486011f2fc873f1fe985503fee3888aab8
Taylor C. Richberger <Taywee@gmx.com>
2015-08-07 16:32:51 -0600

    add existence testing

--- linux/V-38519/fix.sh
+++ linux/V-38519/fix.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 for logfile in $(grep -roE '/[-\/a-zA-Z\._0-9]+($|[[:space:]])' /etc/rsyslog.*); do
-    if ! test_group "$logfile" root >/dev/null 2>&1; then
+    if [ -e "$logfile" ] && ! test_group "$logfile" root >/dev/null 2>&1; then
         chgrp root "$logfile"
     fi
 done
--- linux/V-38519/test.sh
+++ linux/V-38519/test.sh
@@ -2,7 +2,7 @@
 exitstatus=0
 
 for logfile in $(grep -hroE '/[-\/a-zA-Z\._0-9]+($|[[:space:]])' /etc/rsyslog.*); do
-    if ! test_group "$logfile" root; then
+    if [ -e "$logfile" ] && ! test_group "$logfile" root; then
         exitstatus=1
     fi
 done
