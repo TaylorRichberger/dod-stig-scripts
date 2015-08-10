4f973e3a55777305c36f5ea2e0e4a10a9f6801d8
Taylor C. Richberger <Taywee@gmx.com>
2015-08-10 10:58:32 -0600

    update add test

--- linux/V-38674/test.sh
+++ linux/V-38674/test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if ! grep -qE '^id:3:inittedault:' /etc/inittab; then
+if ! grep -qE '^id:3:initdefault:' /etc/inittab; then
     echo Default runlevel is not 3
     exit 1
 fi
--- test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-OS="$(uname -s | tr '[[:upper:]]' '[[:lower:]]')"
-
-./runrules.pl -t ./$OS/rules/*.pm
